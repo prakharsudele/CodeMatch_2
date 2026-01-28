7a8546861b85b29ffd138d561e76e0c9c7adffb7
prakharsudele <prakharsudele36@gmail.com>
2026-01-28 11:06:05 +0530

    auth error

--- server/config/passport.js
+++ server/config/passport.js
@@ -15,35 +15,22 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL,
+      callbackURL: process.env.GITHUB_CALLBACK_URL
     },
-    
     async (accessToken, refreshToken, profile, done) => {
-      try {
-        let user = await User.findOne({ githubId: profile.id });
+      const user = await User.findOneAndUpdate(
+        { githubId: profile.id },
+        {
+          githubId: profile.id,
+          username: profile.username,
+          email: profile.emails?.[0]?.value
+        },
+        { upsert: true, new: true }
+      );
 
-        if (!user) {
-          user = await User.create({
-            githubId: profile.id,
-            username: profile.username,
-            avatar: profile.photos[0].value,
-            email: profile.emails?.[0]?.value,
-          });
-        }
-
-        return done(null, user);
-      } catch (err) {
-        return done(err, null);
-      }
+      return done(null, user);
     }
   )
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
 
-passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
-  done(null, user);
-});
